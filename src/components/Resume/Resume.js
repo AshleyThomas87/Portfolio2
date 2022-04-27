117c49ba5a85e8a798e5080485ded71a12db4438
Tara <tara.brichetto@gmail.com>
2022-04-27 11:43:04 -0700

    resume link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './style.css';
+import PDF from './resume.pdf';
+import './resumestyle.css';
 
 class Resume extends Component {
     render () {
     return (
-  <div>Resume</div>
+      <section className="resumeheader">
+        <h4 className="resumetitle">Resume</h4>
+        <p><a href = {PDF} rel="noreferrer noopener" target="_blank">Click for PDF</a></p>
+    </section>
   
   );
     }
